93aa8cd6ac2b469e83d3c7600d5e32a7a8623d97
Brad Covey <bradcovey@gmail.com>
2017-11-14 11:44:13 -0400

    Added a grid generator and a cell generator

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from shapely.geometry import Polygon
 
 import grid
 
+
 def main():
     bounds = Polygon([
         (84, -34.5000000001998046),
@@ -10,8 +11,9 @@ def main():
         (81.0000000000999876, 41.7812499296405804),
         (81.0000000000999876, -34.5000000001998046)
     ])
-    
-    print(grid.create_grid('Emodnet', from_shape(bounds, srid=4326), 0.002))
+
+    g = grid.create_grid('Emodnet', from_shape(bounds, srid=4326), 0.5)
+    grid.generate_grid_cells(g)
     return
 
 if __name__ == '__main__':
--- grid.py
+++ grid.py
@@ -1,9 +1,17 @@
+import numpy as np
+import pandas as pd
+
+from shapely.geometry import Polygon
+from geoalchemy2.shape import to_shape, from_shape
+
+import progressbar
+
 import models
 
 
 def create_grid(name, bounding_box, cell_size=None, cell_width=None, cell_height=None):
     """Create an entry in the grid table.
-  
+
     Returns
     -------
     grid(Grid): an sqlalchemy ORM entity for the created grid
@@ -23,6 +31,10 @@ def create_grid(name, bounding_box, cell_size=None, cell_width=None, cell_height
     )
 
     session = models.Session()
+    loaded_grid = session.query(models.Grid).filter(models.Grid.name == name).one_or_none()
+    if loaded_grid is not None:
+        session.close()
+        return loaded_grid
     session.add(grid)
     session.commit()
     session.close()
@@ -37,4 +49,33 @@ def generate_grid_cells(grid):
     grid(Grid): an sqlalchemy orm entity for the target grid
 
     """
-    return
+    bbox_shape = to_shape(grid.bounding_box)
+    min_x, min_y, max_x, max_y = bbox_shape.bounds
+
+    x_range = np.arange(min_x, max_x, grid.cell_width)
+    y_range = np.arange(min_y, max_y, grid.cell_height)
+
+    grid_cells = pd.DataFrame(index=y_range, columns=x_range)
+
+    session = models.Session()
+    cell_mappings = []
+    i = 0
+    for y, r in grid_cells.iterrows():
+        for x, v in r.iteritems():
+            bounding_box = Polygon([
+                (x, y),
+                (x + grid.cell_width, y),
+                (x + grid.cell_width, y + grid.cell_height),
+                (x, y + grid.cell_height)
+            ])
+
+            cell_mappings.append(dict(
+                grid_id=grid.id,
+                bounding_box=from_shape(bounding_box)
+            ))
+        i += 1
+        print("%s/%s" % (i, len(grid_cells)))
+    print("Generates %s cells, inserting" % (len(cell_mappings)))
+    session.bulk_insert_mappings(models.GridCell, cell_mappings)
+    session.commit()
+    session.close()
